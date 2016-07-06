5bfbf6d966dccd0163ef656de413a771b5cd8653
Xiaohu Li <li.xiaohu6@gmail.com>
2016-07-05 21:55:46 -0700

    add print_report method

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,4 +1,7 @@
+require 'terminal-table'
+
 module Analyzable
+  @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
 
   def average_price(products)
     result = 0
@@ -7,4 +10,16 @@ module Analyzable
     end
     return (result/products.length).round(2)
   end
+
+  def print_report(products)
+    report = CSV.read(@@data_path)
+    rows = []
+    report.each do |line|
+      rows << line
+    end
+    table = Terminal::Table.new :rows => rows
+    puts table
+    return "tablized report above"
+  end
+
 end
