bf458c7e37bc3f87362861ce34f3ad35e4fbdc18
Xiaohu Li <li.xiaohu6@gmail.com>
2016-07-09 12:53:33 -0700

    analyzable.rb fix

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -8,45 +8,30 @@ module Analyzable
   end
 
   def print_report(products)
-    report = CSV.read(@@data_path)
-    rows = []
-    report.each do |line|
-      rows << line
+    report = "PRINT SUMMARY REPORT\n"
+    report += "--------------------\n"
+    report += "The average price is: "
+    report += "$#{average_price(products)}\n"
+    report += "Stocks by brands:\n"
+    count_by_brand(products).each do |key, value|
+      report += "    - #{key}: #{value}\n"
     end
-    table = Terminal::Table.new :rows => rows
-    puts "Inventory Table:"
-    puts table
-    puts "Stocks by brands:"
-    puts count_by_brand(products)
-    puts "Stocks by names:"
-    puts count_by_name(products)
-    print "The average price is: "
-    puts "$#{average_price(products)}"
-    return "Date: #{Time.now.strftime("%Y-%d-%m")}"
+    report += "Stocks by names:\n"
+    count_by_name(products).each do |key, value|
+      report += "    - #{key}: #{value}\n"
+    end
+    report += "Date: #{Time.now.strftime("%Y-%d-%m")}"
+    return report
   end
 
-  def count_by_brand(products)
-    result = {}
-    products.each do |product|
-      if result.include? product.brand
-        result[product.brand] += 1
-      else
-        result[product.brand] = 1
+  [:brand, :name].each do |field|
+    send(:define_method, "count_by_#{field}") do |products|
+      sums = Hash.new(0)
+      products.each do |product|
+        value = product.send(field)
+        sums[value] += 1
       end
+      sums
     end
-    return result
   end
-
-  def count_by_name(products)
-    result = {}
-    products.each do |product|
-      if result.include? product.name
-        result[product.name] += 1
-      else
-        result[product.name] = 1
-      end
-    end
-    return result
-  end
-
 end
